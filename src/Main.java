d3b74da64af9b4700deadcb8e1980a0797a29cc5
MishMarchenko <mishmar4enko@gmail.com>
2023-06-30 22:16:49 +0300

    Last past HW

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import exception.InvalidDoorsNumberException;
 import exception.InvalidSteeringWheelNumberException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
+        System.out.println("First and Second part HW");
         Car bmv = new Car("BMVx5", 4, 5, 1);
         Car opel = new Car("OpelZafira", 4,5,1);
         try {
@@ -17,5 +21,16 @@ public class Main {
         }
         System.out.println(bmv);
         System.out.println(opel);
+        System.out.println("Third part HW");
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i < 11; i++){
+            list.add(i);
+        }
+        System.out.println(list);
+        list.remove(9);
+        list.remove(8);
+        list.remove(7);
+        list.set(6, 10);
+        System.out.println(list);
     }
 }
